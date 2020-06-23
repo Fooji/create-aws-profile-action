91000aef13b0b6e470e617c9a68d63a9c6a7d2da
William Coates <william@fooji.com>
2020-06-23 18:21:56 +0100

    fix config format

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const core = require('@actions/core');
 const os = require('os')
 const fs = require('fs')
 try {
-  const key = core.getInput('key');
-  const secret = core.getInput('secret');
-  const profile = core.getInput('profile');
-  const region = core.getInput('region');
+  const key = core.getInput('key') || 'you forgot the key';
+  const secret = core.getInput('secret') || 'you forgot the secret';
+  const profile = core.getInput('profile') || 'you forgot the profile';
+  const region = core.getInput('region') || 'us-whatevs-1';
   console.log(`Setting up profile ${profile} in region ${region}...`)
   const awsDir = `${os.homedir()}/.aws`
   const credentials = `${awsDir}/credentials`
@@ -17,7 +17,7 @@ try {
   fs.appendFileSync(credentials, `aws_access_key_id=${key}\n`)
   fs.appendFileSync(credentials, `aws_secret_access_key=${secret}\n`)
   fs.appendFileSync(config, `[profile ${profile}]\n`)
-  fs.appendFileSync(config, `region ${region}\n`)
+  fs.appendFileSync(config, `region=${region}\n`)
 } catch (error) {
   core.setFailed(error.message);
 }
